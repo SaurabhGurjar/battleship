551b00786cdbf8941be7cab1d70b4abfb78801fc
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-04-11 21:58:55 +0530

    feat: Add invalid value check.

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,11 +1,25 @@
 export default class Gameboard {
-    constructor(width, length) {
+    static BOARD_MINIMUM_DIMENSION = 47;
+    static BOARD_MAXIMUM_DIMENSION = 100;
+    constructor(width, height) {
+        if(typeof width !== "number" || typeof height !== "number" || isNaN(width) || isNaN(height)) {
+            throw new Error("dimensions must be numeric values.");
+        }
+        if(width * height <  Gameboard.BOARD_MINIMUM_DIMENSION) {
+            throw new Error("provide dimensions of at least (7 X 7) to create a board.");
+        }
+        if(width * height > Gameboard.BOARD_MAXIMUM_DIMENSION) {
+            throw new Error("provide dimensions up to a maximum of (10 X 10) to create a board.")
+        }
+        if(!Number.isInteger(width) || !Number.isInteger(height)) {
+            throw new Error("dimensions must be integers.");
+        }
         this.board = [];
         this.boardWidth = width;
-        this.boardLength = length;
+        this.boardheight = height;
     }
 
     get length() {
-        return this.boardWidth * this.boardLength;
+        return this.boardWidth * this.boardheight;
     }
 }
