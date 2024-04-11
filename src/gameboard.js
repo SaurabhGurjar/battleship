0bf3d1ce663aef030c8feaf070951ee8a44f69d6
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-04-12 01:31:48 +0530

    fix: Indentation and spacing.

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,25 +1,34 @@
 export default class Gameboard {
-    static BOARD_MINIMUM_DIMENSION = 47;
-    static BOARD_MAXIMUM_DIMENSION = 100;
-    constructor(width, height) {
-        if(typeof width !== "number" || typeof height !== "number" || isNaN(width) || isNaN(height)) {
-            throw new Error("dimensions must be numeric values.");
-        }
-        if(width * height <  Gameboard.BOARD_MINIMUM_DIMENSION) {
-            throw new Error("provide dimensions of at least (7 X 7) to create a board.");
-        }
-        if(width * height > Gameboard.BOARD_MAXIMUM_DIMENSION) {
-            throw new Error("provide dimensions up to a maximum of (10 X 10) to create a board.")
-        }
-        if(!Number.isInteger(width) || !Number.isInteger(height)) {
-            throw new Error("dimensions must be integers.");
-        }
-        this.board = [];
-        this.boardWidth = width;
-        this.boardheight = height;
+  static BOARD_MINIMUM_DIMENSION = 47;
+  static BOARD_MAXIMUM_DIMENSION = 100;
+  constructor(width, height) {
+    if (
+      typeof width !== "number" ||
+      typeof height !== "number" ||
+      isNaN(width) ||
+      isNaN(height)
+    ) {
+      throw new Error("dimensions must be numeric values.");
     }
+    if (width * height < Gameboard.BOARD_MINIMUM_DIMENSION) {
+      throw new Error(
+        "provide dimensions of at least (7 X 7) to create a board.",
+      );
+    }
+    if (width * height > Gameboard.BOARD_MAXIMUM_DIMENSION) {
+      throw new Error(
+        "provide dimensions up to a maximum of (10 X 10) to create a board.",
+      );
+    }
+    if (!Number.isInteger(width) || !Number.isInteger(height)) {
+      throw new Error("dimensions must be integers.");
+    }
+    this.shipsOnBoard = new Map();
+    this.boardWidth = width;
+    this.boardheight = height;
+  }
 
-    get length() {
-        return this.boardWidth * this.boardheight;
-    }
+  get length() {
+    return this.boardWidth * this.boardheight;
+  }
 }
