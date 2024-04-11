0bb712c68205eeb007361e3bfc1e0a1c06d7bd7b
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-04-11 15:25:46 +0530

    test: Add tests for ships module.

---
+++ tests/ship.test.js
@@ -0,0 +1,86 @@
+import Ship from "../src/ships";
+
+describe("create a ship", () => {
+  const ship1 = {
+    length: 1,
+    hits: 0,
+  };
+  const ship5 = {
+    length: 5,
+    hits: 0,
+  };
+  test("ship of length -1", () => {
+    expect(() => new Ship(0)).toThrow(
+      `ship's length must be less than 6 and greater than 0.`,
+    );
+  });
+  test("ship of length one", () => {
+    expect(new Ship(1)).toEqual(ship1);
+  });
+  test("ship of length five", () => {
+    expect(new Ship(5)).toEqual(ship5);
+  });
+  test("ship of length six", () => {
+    expect(() => new Ship(6)).toThrow(
+      `ship's length must be less than 6 and greater than 0.`,
+    );
+  });
+  test("ship of length zero", () => {
+    expect(() => new Ship(0)).toThrow(
+      `ship's length must be less than 6 and greater than 0.`,
+    );
+  });
+  describe("invalid length value", () => {
+    test("'1'", () => {
+      expect(() => new Ship("1")).toThrow(
+        "'1' is not a valid for ship length, The arg must be a number.",
+      );
+    });
+    test("NaN", () => {
+      expect(() => new Ship(NaN)).toThrow(
+        "'NaN' is not a valid for ship length, The arg must be a number.",
+      );
+    });
+    test("undefined", () => {
+      expect(() => new Ship()).toThrow("the length of the ship is undefined.");
+    });
+    test("null", () => {
+      expect(() => new Ship(null)).toThrow(
+        "'null' is not a valid for ship length, The arg must be a number.",
+      );
+    });
+    test("true", () => {
+      expect(() => new Ship(true)).toThrow(
+        "'true' is not a valid for ship length, The arg must be a number.",
+      );
+    });
+  });
+});
+
+describe("hit the ship", () => {
+  let ship;
+  // Use beforeEach hook to intailize the ship before each test
+  beforeEach(() => {
+    ship = new Ship(3);
+  });
+  // const ship = new Ship(3);
+  test("hit a ship until it sunk", () => {
+    ship.hit();
+    expect(ship.hits).toBe(1);
+    expect(ship.isSunk()).toBeFalsy();
+    ship.hit();
+    expect(ship.hits).toBe(2);
+    expect(ship.isSunk()).toBeFalsy();
+    ship.hit();
+    expect(ship.hits).toBe(3);
+    expect(ship.isSunk()).toBeTruthy();
+  });
+  test("hit a ship beyond its length", () => {
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.hits).toBe(3);
+    expect(ship.isSunk()).toBeTruthy();
+  });
+});
