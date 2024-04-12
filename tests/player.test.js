9e3e0825dd49337b1c7015d074cf58e8ee5d302b
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-04-12 16:42:44 +0530

    test: Add unit tests for player class.

---
+++ tests/player.test.js
@@ -0,0 +1,60 @@
+import Player from "../src/player";
+import Gameboard from "../src/gameboard";
+
+describe("create a new player", () => {
+  const p1 = new Player("p1", new Gameboard(8, 8));
+  const p2 = new Player("p2", new Gameboard(8, 8));
+
+  const player1 = {
+    name: "p1",
+    _turn: false,
+    gameboard: new Gameboard(8, 8),
+  };
+
+  const player2 = {
+    name: "p2",
+    _turn: false,
+    gameboard: new Gameboard(8, 8),
+  };
+  test("player 1", () => {
+    expect(p1).toEqual(player1);
+  });
+
+  test("player 2", () => {
+    expect(p2).toEqual(player2);
+  });
+
+  test("player1's turn to shot", () => {
+    p1.turn = true;
+    expect(p1.isTurn).toBeTruthy();
+  });
+  test("player2's turn to shot", () => {
+    p2.turn = true;
+    p1.turn = false;
+    expect(p1.isTurn).toBeFalsy();
+    expect(p2.isTurn).toBeTruthy();
+  });
+
+  test("get gameboard of player 1", () => {
+    expect(p1.gameboard).toEqual(player1.gameboard);
+  });
+
+  describe("invalid input values", () => {
+    test("name is type of number", () => {
+      expect(() => new Player(2, new Gameboard(8, 8))).toThrow(
+        "name must be a string.",
+      );
+    });
+    test("gameboard must be an instanceof Gameboard", () => {
+      expect(
+        () =>
+          new Player("p2", {
+            boardWidth: 8,
+            boardheight: 8,
+            missed: new Set(),
+            shipsOnBoard: new Map(),
+          }),
+      ).toThrow("gameboard must be an instances of Gameboard.");
+    });
+  });
+});
