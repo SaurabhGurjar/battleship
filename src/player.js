9a93c20fcb96ab6aca855fecb65c4eb3999d35f5
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-04-12 19:20:52 +0530

    feat: Add  Property that store ships locations and setShipLoc method to add new ship and its coordinates.

--- src/player.js
+++ src/player.js
@@ -10,6 +10,11 @@ export default class Player {
     this.name = name;
     this._turn = false;
     this.gameboard = gameBoard;
+    this.shipsPos = new Map();
+  }
+
+  setShipLoc(ship, coords) {
+    this.shipsPos.set(ship, coords);
   }
 
   set turn(bool) {
