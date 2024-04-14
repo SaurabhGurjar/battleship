9c16d2e55c1144c6a89db4cc366c89881e7e67b5
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-04-14 14:24:24 +0530

    feat: Implement a function to create the boards.

--- src/createUI.js
+++ src/createUI.js
@@ -1,8 +1,12 @@
-const root = document.querySelector("#root");
+const p1Name = document.querySelector("#p1-name");
+const p2Name = document.querySelector("#p2-name");
+const p1BoardC = document.querySelector("#p1-c");
+const p2BoardC = document.querySelector("#p2-c");
 
 function createPlayerBoard(player) {
+  let count = 1;
   const boardUI = document.createElement("div");
-  boardUI.id = player.name;
+  boardUI.id = player.name.split(" ").join("-");
   boardUI.classList.add("board");
   for (let row = 1; row <= player.board.boardWidth; row++) {
     const rowDiv = document.createElement("div");
@@ -13,7 +17,8 @@ function createPlayerBoard(player) {
       const colDiv = document.createElement("div");
       colDiv.id = `c-${col}`;
       colDiv.classList.add("col");
-      colDiv.textContent = `${row}-${col}`;
+      colDiv.textContent = count;
+      count++;
       rowDiv.appendChild(colDiv);
     }
     boardUI.appendChild(rowDiv);
@@ -21,7 +26,9 @@ function createPlayerBoard(player) {
   return boardUI;
 }
 
-export default function createUI(player) {
-  console.log(player.board);
-  root.appendChild(createPlayerBoard(player));
+export default function createUI(player1, player2) {
+  p1Name.textContent = player1.name;
+  p2Name.textContent = player2.name;
+  p1BoardC.appendChild(createPlayerBoard(player1));
+  p2BoardC.appendChild(createPlayerBoard(player2));
 }
