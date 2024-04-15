f419b81915d50bd3b593133a8eacb3d3c8312bdd
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-04-15 22:05:39 +0530

    feat: Add event listener on each cell.

--- src/createUI.js
+++ src/createUI.js
@@ -1,9 +1,21 @@
+import { $ } from "./utils/dom-utils";
+import { capitalize } from "./utils/string-utils";
+import { getAttackCoord } from "./utils/ui-utils";
+
 const p1Name = document.querySelector("#p1-name");
 const p2Name = document.querySelector("#p2-name");
 const p1BoardC = document.querySelector("#p1-c");
 const p2BoardC = document.querySelector("#p2-c");
 
-function createPlayerBoard(player) {
+function highLightShip(player) {
+  const playerId = player.name.split(" ").join("-");
+  const locationsIter = player.gameboard.shipLocation.keys();
+  for (const val of locationsIter) {
+    $(`#${playerId}-${val}`).classList.add("show-ships");
+  }
+}
+
+function createPlayerBoard(player, callback) {
   let count = 1;
   const boardUI = document.createElement("div");
   boardUI.id = player.name.split(" ").join("-");
@@ -13,13 +25,16 @@ function createPlayerBoard(player) {
     rowDiv.id = `r-${row}`;
     rowDiv.classList.add("row");
 
-    for (let col = 1; col <= player.board.boardheight; col++) {
-      const colDiv = document.createElement("div");
-      colDiv.id = `c-${col}`;
-      colDiv.classList.add("col");
-      colDiv.textContent = count;
+    for (let cell = 1; cell <= player.board.boardheight; cell++) {
+      const cellDiv = document.createElement("div");
+      cellDiv.id = `${boardUI.id}-${count}`;
+      cellDiv.classList.add("cell");
+      cellDiv.dataset.pos = count;
+      cellDiv.onclick = () => {
+        callback(cellDiv, player);
+      }
       count++;
-      rowDiv.appendChild(colDiv);
+      rowDiv.appendChild(cellDiv);
     }
     boardUI.appendChild(rowDiv);
   }
@@ -27,8 +42,9 @@ function createPlayerBoard(player) {
 }
 
 export default function createUI(player1, player2) {
-  p1Name.textContent = player1.name;
-  p2Name.textContent = player2.name;
-  p1BoardC.appendChild(createPlayerBoard(player1));
-  p2BoardC.appendChild(createPlayerBoard(player2));
+  p1Name.textContent = capitalize(player1.name);
+  p2Name.textContent = capitalize(player2.name);
+  p1BoardC.appendChild(createPlayerBoard(player1, getAttackCoord));
+  p2BoardC.appendChild(createPlayerBoard(player2, getAttackCoord));
+  highLightShip(player1);
 }
