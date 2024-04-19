306a7b47fd3cc1dbddda3777ccf140ebc9a2a8cb
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-04-19 21:45:17 +0530

    feat: Add a method to create an id from player name.

--- src/utils/string-utils.js
+++ src/utils/string-utils.js
@@ -2,3 +2,7 @@ export function capitalize(str) {
   const strArr = str.split("");
   return [].concat([strArr[0].toUpperCase()], strArr.slice(1)).join("");
 }
+
+export function createId(name) {
+  return name.split(" ").join("");
+}
