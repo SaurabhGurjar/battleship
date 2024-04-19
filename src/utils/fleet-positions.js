12873b85088e388cd9973efa0755807389eb13c4
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-04-20 00:56:17 +0530

    feat: Add some fleet positions.

---
+++ src/utils/fleet-positions.js
@@ -0,0 +1,230 @@
+export const fleetPositions = [
+  [
+    {
+      start: 89,
+      end: 89,
+    },
+    {
+      start: 44,
+      end: 44,
+    },
+    {
+      start: 19,
+      end: 19,
+    },
+    {
+      start: 73,
+      end: 73,
+    },
+    {
+      start: 59,
+      end: 69,
+    },
+    {
+      start: 85,
+      end: 86,
+    },
+    {
+      start: 46,
+      end: 66,
+    },
+    {
+      start: 25,
+      end: 27,
+    },
+    {
+      start: 22,
+      end: 52,
+    },
+  ],
+  [
+    {
+      start: 93,
+      end: 93,
+    },
+    {
+      start: 89,
+      end: 89,
+    },
+    {
+      start: 32,
+      end: 32,
+    },
+    {
+      start: 68,
+      end: 68,
+    },
+    {
+      start: 19,
+      end: 29,
+    },
+    {
+      start: 62,
+      end: 72,
+    },
+    {
+      start: 76,
+      end: 96,
+    },
+    {
+      start: 45,
+      end: 47,
+    },
+    {
+      start: 13,
+      end: 16,
+    },
+  ],
+  [
+    {
+      start: 72,
+      end: 72,
+    },
+    {
+      start: 76,
+      end: 76,
+    },
+    {
+      start: 32,
+      end: 32,
+    },
+    {
+      start: 39,
+      end: 39,
+    },
+    {
+      start: 8,
+      end: 9,
+    },
+    {
+      start: 74,
+      end: 84,
+    },
+    {
+      start: 13,
+      end: 15,
+    },
+    {
+      start: 45,
+      end: 47,
+    },
+    {
+      start: 69,
+      end: 99,
+    },
+  ],
+  [
+    {
+      start: 2,
+      end: 2,
+    },
+    {
+      start: 36,
+      end: 36,
+    },
+    {
+      start: 83,
+      end: 83,
+    },
+    {
+      start: 89,
+      end: 89,
+    },
+    {
+      start: 15,
+      end: 16,
+    },
+    {
+      start: 86,
+      end: 96,
+    },
+    {
+      start: 55,
+      end: 57,
+    },
+    {
+      start: 32,
+      end: 52,
+    },
+    {
+      start: 9,
+      end: 39,
+    },
+  ],
+  [
+    {
+      start: 98,
+      end: 98,
+    },
+    {
+      start: 94,
+      end: 94,
+    },
+    {
+      start: 3,
+      end: 3,
+    },
+    {
+      start: 69,
+      end: 69,
+    },
+    {
+      start: 28,
+      end: 29,
+    },
+    {
+      start: 22,
+      end: 23,
+    },
+    {
+      start: 74,
+      end: 76,
+    },
+    {
+      start: 6,
+      end: 8,
+    },
+    {
+      start: 44,
+      end: 47,
+    },
+  ],
+  [
+    {
+      start: 6,
+      end: 6,
+    },
+    {
+      start: 51,
+      end: 51,
+    },
+    {
+      start: 48,
+      end: 48,
+    },
+    {
+      start: 86,
+      end: 86,
+    },
+    {
+      start: 73,
+      end: 83,
+    },
+    {
+      start: 9,
+      end: 19,
+    },
+    {
+      start: 13,
+      end: 33,
+    },
+    {
+      start: 79,
+      end: 99,
+    },
+    {
+      start: 36,
+      end: 66,
+    },
+  ],
+];
